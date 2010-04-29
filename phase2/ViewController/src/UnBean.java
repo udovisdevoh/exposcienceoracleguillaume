4dec640b9f06de24021c250e44dab03d18d34bac
glacasse <glacasse@31b38a5c-07a4-8787-1137-4cbf53c509f6>
2010-04-29 21:43:41 +0000

    Changement de mot de passe: update ne marche pas encore car on ne peut pas avoir le nom de l'usager en provenance du pageFlowScope. Le pageFlowScope retourné dans UnBean à une cardinalité de 0.

---
+++ phase2/ViewController/src/UnBean.java
@@ -0,0 +1,53 @@
+import java.util.Map;
+
+import javax.faces.context.FacesContext;
+
+import oracle.binding.BindingContainer;
+import oracle.binding.OperationBinding;
+
+import org.apache.myfaces.trinidad.context.RequestContext;
+
+public class UnBean
+{
+  private BindingContainer bindings;
+
+  public UnBean()
+  {
+  }
+
+  public BindingContainer getBindings()
+  {
+    if (this.bindings == null)
+    {
+      FacesContext fc = FacesContext.getCurrentInstance();
+      this.bindings =
+          (BindingContainer) fc.getApplication().evaluateExpressionGet(fc, "#{bindings}",
+            BindingContainer.class);
+    }
+    return this.bindings;
+  }
+
+  public String commandButton_action()
+  {
+    Map pageFlowScope = RequestContext.getCurrentInstance().getPageFlowScope();
+    
+    
+    System.out.println("Il y a " + pageFlowScope.size() + " item dans le pageFlowScope");
+    
+    Object myObject = pageFlowScope.get("codeUsager");
+    
+    if (myObject == null)
+      System.out.println("Attention, l'objet retourné du pageFlowScope est null");   
+    
+    System.out.println(myObject);   
+    
+    BindingContainer bindings = getBindings();
+    OperationBinding operationBinding = bindings.getOperationBinding("Commit");
+    Object result = operationBinding.execute();
+    if (!operationBinding.getErrors().isEmpty())
+    {
+      return null;
+    }
+    return null;
+  }
+}
