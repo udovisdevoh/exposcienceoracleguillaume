cbbf5065c4e856c7ca21b9e4de96ea658ba243b2
glacasse <glacasse@31b38a5c-07a4-8787-1137-4cbf53c509f6>
2010-05-17 16:28:34 +0000

    Ajouté: si pas juge: ne peut pas cliquer sur lien pour évaluation

--- phase3/Model/src/model/view/PAuditeurViewImpl.java
+++ phase3/Model/src/model/view/PAuditeurViewImpl.java
@@ -13,187 +13,190 @@ import oracle.jbo.server.ViewObjectImpl;
 // ---    Custom code may be added to this class.
 // ---    Warning: Do not modify method signatures of generated methods.
 // ---------------------------------------------------------------------
-public class PAuditeurViewImpl
-  extends ViewObjectImpl
-  implements PAuditeurView
+public class PAuditeurViewImpl extends ViewObjectImpl implements PAuditeurView
 {
-  /**
-   * This is the default constructor (do not remove).
-   */
-  public PAuditeurViewImpl()
-  {
-  }
-
-  public boolean connecter(String userName, String password)
-  {
-    System.out.println("Methode connecter avec " + userName + " et " +
-                       password);
-    setuserName(userName);
-    setpassWord(password);
-    executeQuery();
-    return (getRowCount() > 0);
-  }
-
-  /**
-   * Returns the bind variable value for userName.
-   * @return bind variable value for userName
-   */
-  public String getuserName()
-  {
-    return (String) getNamedWhereClauseParam("userName");
-  }
-
-  /**
-   * Sets <code>value</code> for bind variable userName.
-   * @param value value to bind as userName
-   */
-  public void setuserName(String value)
-  {
-    setNamedWhereClauseParam("userName", value);
-  }
-
-  /**
-   * Returns the bind variable value for passWord.
-   * @return bind variable value for passWord
-   */
-  public String getpassWord()
-  {
-    return (String) getNamedWhereClauseParam("passWord");
-  }
-
-  /**
-   * Sets <code>value</code> for bind variable passWord.
-   * @param value value to bind as passWord
-   */
-  public void setpassWord(String value)
-  {
-    setNamedWhereClauseParam("passWord", value);
-  }
-
-  public boolean modifierMotDePasse(String ancienMotDePasse,
-                                    String nouveauMotDePasse,
-                                    String confirmationMotDePasse)
-  {
-    System.out.println("Methode modifier mot de passe");
-    System.out.println("Ancien: " + ancienMotDePasse);
-    System.out.println("Nouveau1: " + nouveauMotDePasse);
-    System.out.println("Nouveau2: " + confirmationMotDePasse);
-
-    PAuditeurViewRowImpl row =
-      (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
-    String password = row.getMotdepasse();
-
-    System.out.println("Ancien mot de passe dans la row trouvée: " +
-                       password);
-
-    if (password.equals(ancienMotDePasse))
+    /**
+     * This is the default constructor (do not remove).
+     */
+    public PAuditeurViewImpl()
     {
-      System.out.println("Yé! l'ancien mot de passe est bon");
-      if (nouveauMotDePasse.equals(confirmationMotDePasse))
-      {
-        System.out.println("Yé! Les deux nouvaux mots de passes sont pareils!");
-        row.setMotdepasse(nouveauMotDePasse);
+    }
+
+    public boolean connecter(String userName, String password)
+    {
+        System.out.println("Methode connecter avec " + userName + " et " +
+                           password);
+        setuserName(userName);
+        setpassWord(password);
+        executeQuery();
+        return (getRowCount() > 0);
+    }
+
+    /**
+     * Returns the bind variable value for userName.
+     * @return bind variable value for userName
+     */
+    public String getuserName()
+    {
+        return (String) getNamedWhereClauseParam("userName");
+    }
+
+    /**
+     * Sets <code>value</code> for bind variable userName.
+     * @param value value to bind as userName
+     */
+    public void setuserName(String value)
+    {
+        setNamedWhereClauseParam("userName", value);
+    }
+
+    /**
+     * Returns the bind variable value for passWord.
+     * @return bind variable value for passWord
+     */
+    public String getpassWord()
+    {
+        return (String) getNamedWhereClauseParam("passWord");
+    }
+
+    /**
+     * Sets <code>value</code> for bind variable passWord.
+     * @param value value to bind as passWord
+     */
+    public void setpassWord(String value)
+    {
+        setNamedWhereClauseParam("passWord", value);
+    }
+
+    public boolean modifierMotDePasse(String ancienMotDePasse,
+                                      String nouveauMotDePasse,
+                                      String confirmationMotDePasse)
+    {
+        System.out.println("Methode modifier mot de passe");
+        System.out.println("Ancien: " + ancienMotDePasse);
+        System.out.println("Nouveau1: " + nouveauMotDePasse);
+        System.out.println("Nouveau2: " + confirmationMotDePasse);
+
+        PAuditeurViewRowImpl row =
+            (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
+        String password = row.getMotdepasse();
+
+        System.out.println("Ancien mot de passe dans la row trouvée: " +
+                           password);
+
+        if (password.equals(ancienMotDePasse))
+        {
+            System.out.println("Yé! l'ancien mot de passe est bon");
+            if (nouveauMotDePasse.equals(confirmationMotDePasse))
+            {
+                System.out.println("Yé! Les deux nouvaux mots de passes sont pareils!");
+                row.setMotdepasse(nouveauMotDePasse);
+                AppModuleImpl appModuleImpl =
+                    (AppModuleImpl) getApplicationModule();
+                appModuleImpl.getDBTransaction().commit();
+
+                System.out.println("On change le mot de passe pour " +
+                                   nouveauMotDePasse);
+
+                return true;
+            }
+            else
+            {
+                System.out.println("Boo! les 2 nouveaux mots de passes ne matchent pas");
+            }
+        }
+        else
+        {
+            System.out.println("Boo! l'ancien mot de passe n'est pas bon");
+        }
+        return false;
+    }
+
+    public String getDateDevenuCandidatJuge()
+    {
+        PAuditeurViewRowImpl row =
+            (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
+        Date date = row.getCandidatjuge();
+        if (date == null)
+            return "";
+        else
+            return date.toString();
+    }
+
+    public String getDateDevenuJuge()
+    {
+        PAuditeurViewRowImpl row =
+            (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
+        Date date = row.getJuge();
+        if (date == null)
+            return "";
+        else
+            return date.toString();
+    }
+
+    public boolean annulerCandidatureJuge()
+    {
+        PAuditeurViewRowImpl row =
+            (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
+        row.setJuge(null);
         AppModuleImpl appModuleImpl =
-          (AppModuleImpl) getApplicationModule();
+            (AppModuleImpl) getApplicationModule();
+        appModuleImpl.getDBTransaction().commit();
+        return true;
+    }
+
+    public boolean annulerDemandeJuge()
+    {
+        PAuditeurViewRowImpl row =
+            (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
+        row.setCandidatjuge(null);
+        AppModuleImpl appModuleImpl =
+            (AppModuleImpl) getApplicationModule();
+        appModuleImpl.getDBTransaction().commit();
+        return true;
+    }
+
+    public boolean soumettreCandidatureJuge()
+    {
+        PAuditeurViewRowImpl row =
+            (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
+        Date date = (Date) Date.getCurrentDate();
+        row.setCandidatjuge(date);
+        AppModuleImpl appModuleImpl =
+            (AppModuleImpl) getApplicationModule();
+        appModuleImpl.getDBTransaction().commit();
+        return true;
+    }
+
+    public boolean editProfile(String nom, String prenom, String rue,
+                               String ville, String codePostal,
+                               String telephone, String courriel)
+    {
+        PAuditeurViewRowImpl row =
+            (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
+        row.setNom(nom);
+        row.setPrenom(prenom);
+        row.setRue(rue);
+        row.setVille(ville);
+        row.setCodePostal(codePostal);
+        row.setTelephone(telephone);
+        row.setCourriel(courriel);
+
+        AppModuleImpl appModuleImpl =
+            (AppModuleImpl) getApplicationModule();
         appModuleImpl.getDBTransaction().commit();
 
-        System.out.println("On change le mot de passe pour " +
-                           nouveauMotDePasse);
-
         return true;
-      }
-      else
-      {
-        System.out.println("Boo! les 2 nouveaux mots de passes ne matchent pas");
-      }
     }
-    else
+
+    public boolean preEditProfile()
     {
-      System.out.println("Boo! l'ancien mot de passe n'est pas bon");
+        System.out.println("Information personnelles seront modifiées");
+        PAuditeurViewRowImpl row =
+            (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
+        AppModuleImpl appModuleImpl =
+            (AppModuleImpl) getApplicationModule();
+        appModuleImpl.getDBTransaction().commit();
+        return true;
     }
-    return false;
-  }
-
-  public String getDateDevenuCandidatJuge()
-  {
-    PAuditeurViewRowImpl row =
-      (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
-    Date date = row.getCandidatjuge();
-    if (date == null)
-      return "";
-    else
-      return date.toString();
-  }
-
-  public String getDateDevenuJuge()
-  {
-    PAuditeurViewRowImpl row =
-      (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
-    Date date = row.getJuge();
-    if (date == null)
-      return "";
-    else
-      return date.toString();
-  }
-
-  public boolean annulerCandidatureJuge()
-  {
-    PAuditeurViewRowImpl row =
-      (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
-    row.setJuge(null);
-    AppModuleImpl appModuleImpl = (AppModuleImpl) getApplicationModule();
-    appModuleImpl.getDBTransaction().commit();
-    return true;
-  }
-
-  public boolean annulerDemandeJuge()
-  {
-    PAuditeurViewRowImpl row =
-      (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
-    row.setCandidatjuge(null);
-    AppModuleImpl appModuleImpl = (AppModuleImpl) getApplicationModule();
-    appModuleImpl.getDBTransaction().commit();
-    return true;
-  }
-
-  public boolean soumettreCandidatureJuge()
-  {
-    PAuditeurViewRowImpl row =
-      (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
-    Date date = (Date) Date.getCurrentDate();
-    row.setCandidatjuge(date);
-    AppModuleImpl appModuleImpl = (AppModuleImpl) getApplicationModule();
-    appModuleImpl.getDBTransaction().commit();
-    return true;
-  }
-
-  public boolean editProfile(String nom, String prenom, String rue,
-                             String ville, String codePostal,
-                             String telephone, String courriel)
-  {
-    PAuditeurViewRowImpl row =
-      (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
-    row.setNom(nom);
-    row.setPrenom(prenom);
-    row.setRue(rue);
-    row.setVille(ville);
-    row.setCodePostal(codePostal);
-    row.setTelephone(telephone);
-    row.setCourriel(courriel);
-
-    AppModuleImpl appModuleImpl = (AppModuleImpl) getApplicationModule();
-    appModuleImpl.getDBTransaction().commit();
-
-    return true;
-  }
-
-  public boolean preEditProfile()
-  {
-    System.out.println("Information personnelles seront modifiées");
-    PAuditeurViewRowImpl row =
-      (PAuditeurViewRowImpl) this.getRowAtRangeIndex(0);
-    AppModuleImpl appModuleImpl = (AppModuleImpl) getApplicationModule();
-    appModuleImpl.getDBTransaction().commit();
-    return true;
-  }
 }
